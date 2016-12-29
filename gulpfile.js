1c783decb08da49557fc477b21e1a4b2dfac81bc
GrantBartlett <hello@grant-bartlett.com>
2016-12-29 13:29:06 +0000

    add gulp for build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var watch = require('gulp-watch');
  
 gulp.task('default', function () {
     return gulp.src('cncnet/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            target: 'ES5',
+            out: 'cncnet-twitch-app.js'
+        }))
+        .pipe(gulp.dest('example/'));
+});
+
+gulp.task('watch', function () {
+    return watch('cncnet/**/*.ts', { ignoreInitial: false })
         .pipe(ts({
             noImplicitAny: true,
             target: 'ES5',
